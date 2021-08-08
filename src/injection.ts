c6ceafbedb637cd22a7bed222fe9529cdaf1d7a8
Cobular <22972550+JakeCover@users.noreply.github.com>
2021-08-07 17:12:09 -0700

    minimum viable version, events half work

---
+++ src/injection.ts
@@ -0,0 +1,103 @@
+function getDropdown(): Element {
+  return document.getElementsByClassName("form-group col-sm-3 text-center")[0]
+    .children[1]
+}
+
+function injectControl(id: string) {
+  const dropdown: Element = getDropdown()
+  const h4Elements = Array.from(dropdown.children).filter((element) => {
+    return element.tagName.toLowerCase() === "h4"
+  })
+  const slider = dropdown.children[1].cloneNode(true)
+  if (!nodeIsElement(slider)) return
+  // @ts-ignore
+  slider.onclick = toggleButtonState
+  slider.children[0].setAttribute("id", id)
+  const label = slider.children[1]
+  label.setAttribute("for", id)
+  label.textContent = "Not Downloaded"
+  dropdown.insertBefore(slider, h4Elements[1])
+}
+
+let DOWNLOADED_SONGS_BUTTON_STATE: boolean = false
+let DOWNLOADED_SONGS_BUTTON_STATE_HELPER: boolean = false
+
+const showDownloadedSongsEvent = new Event("showDownloadedSongsEvent", {
+  bubbles: true,
+})
+const hideDownloadedSongsEvent = new Event("hideDownloadedSongsEvent", {
+  bubbles: true,
+})
+
+function toggleButtonState() {
+  if (DOWNLOADED_SONGS_BUTTON_STATE_HELPER) {
+    // State Bullshit
+    DOWNLOADED_SONGS_BUTTON_STATE = !DOWNLOADED_SONGS_BUTTON_STATE
+    DOWNLOADED_SONGS_BUTTON_STATE_HELPER = false
+    // Event Firing
+    if (DOWNLOADED_SONGS_BUTTON_STATE)
+      Array.from(document.getElementsByClassName("beatmap")).forEach((value) =>
+        value.dispatchEvent(hideDownloadedSongsEvent)
+      )
+    else
+      Array.from(document.getElementsByClassName("beatmap")).forEach((value) =>
+        value.dispatchEvent(showDownloadedSongsEvent)
+      )
+  } else DOWNLOADED_SONGS_BUTTON_STATE_HELPER = true
+}
+
+function processSearchResults(element: HTMLElement) {
+  element.addEventListener(
+    "hideDownloadedSongsEvent",
+    function () {
+      console.log("hiding")
+      element.style.display = "none"
+    },
+    true)
+
+  element.addEventListener(
+    "showDownloadedSongsEvent",
+    function () {
+      console.log("revealing")
+      element.style.display = "flex"
+    },
+    true
+  )
+}
+
+// function setHashData(element: HTMLElement) {
+//   const link = element.children[2].children[0]
+//   if (!elementIsLink(link)) return
+//   element.dataset.test = link.href
+// }
+
+const observer = new MutationObserver(function (mutations) {
+  for (const mutation of mutations) {
+    for (const node of mutation.addedNodes) {
+      if (nodeIsElement(node) && node.className === "search-results")
+        for (const element of node.children) {
+          if (!nodeIsElement(element)) return
+          processSearchResults(element)
+          // setHashData(element)
+        }
+    }
+  }
+})
+
+const observerParent = document.getElementById("root")
+observer.observe(observerParent, {
+  attributes: true,
+  characterData: true,
+  childList: true,
+  subtree: true,
+})
+injectControl("test")
+
+// Guards
+function nodeIsElement(node: Node): node is HTMLElement {
+  return node instanceof HTMLElement
+}
+
+function elementIsLink(element: Element): element is HTMLLinkElement {
+  return element instanceof HTMLLinkElement
+}
